d0b534373a14e0f2244a66cdc18337da335d8db4
carl wheezer with dreads <147189723+TheEnginnerFromTF2@users.noreply.github.com>
2025-09-24 09:38:12 -0700

    Add files via upload

---
+++ orientation/init.lua
@@ -0,0 +1,13 @@
+---@class orientation
+---@field public UP number the upper direction.
+---@field public RIGHT number direction of the right
+---@field public DOWN number the downwards direction
+---@field public LEFT number direction of the left
+local orientation = {
+    UP = 0,
+    RIGHT = 1,
+    DOWN = 2,
+    LEFT = 3
+}
+
+return orientation
---
+++ runloop/init.lua
@@ -0,0 +1,9 @@
+local runloop = {}
+
+---@param func function The functions to run
+function runloop.run(func) end
+
+---@param duration int The duration in milliseconds
+function runloop.sleep_ms(duration) end
+
+return runloop
